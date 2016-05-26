7ce1db7ebd5819dcc62f571c9edc80603cdcfdf6
Yu Jing <yujing5b5d@gmail.com>
2016-05-26 22:41:11 +0800

    update confiuration

--- project/BuildValhalla.scala
+++ project/BuildValhalla.scala
@@ -1,14 +1,19 @@
 import sbt.Keys._
 import sbt._
+import sbtassembly.AssemblyKeys._
+import sbtassembly.{ MergeStrategy, PathList }
 
 object BuildValhalla extends Build {
-  lazy val id = "valhalla" // valhalla
+  lazy val id = "valhalla"
+  lazy val projVersion = "0.0.1"
+  lazy val projOrganization = "com.argcv"
+  lazy val projScalaVersion = "2.11.8"
 
   lazy val commonSettings = Seq(
     name := id,
-    version := "0.0.1",
-    organization := "com.argcv",
-    scalaVersion := "2.11.7",
+    version := projVersion,
+    organization := projOrganization,
+    scalaVersion := projScalaVersion,
     licenses := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
     homepage := Some(url("https://github.com/yuikns/valhalla"))
   )
@@ -44,16 +49,39 @@ object BuildValhalla extends Build {
       "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
     ),
     dependencyOverrides ++= Set(
-      "org.scala-lang" % "scala-reflect" % "2.11.7",
-      "org.scala-lang" % "scala-compiler" % "2.11.7",
-      "org.scala-lang" % "scala-library" % "2.11.7",
+      "org.scala-lang" % "scala-reflect" % projScalaVersion,
+      "org.scala-lang" % "scala-compiler" % projScalaVersion,
+      "org.scala-lang" % "scala-library" % projScalaVersion,
       "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.4"
     )
+  )
+
+  lazy val assemblySettings = Seq(
+    assemblyJarName in assembly := s"$id-$projVersion-$projScalaVersion.jar",
+    assemblyMergeStrategy in assembly := {
+      case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+      case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+      case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+      case PathList("com", "google", xs @ _*) => MergeStrategy.last
+      case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+      case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+      case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+      case "about.html" => MergeStrategy.rename
+      case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+      case "META-INF/mailcap" => MergeStrategy.last
+      case "META-INF/mimetypes.default" => MergeStrategy.last
+      case "plugin.properties" => MergeStrategy.last
+      case "log4j.properties" => MergeStrategy.last
+      case x =>
+        val oldStrategy = (assemblyMergeStrategy in assembly).value
+        oldStrategy(x)
+    }
   )
 
   lazy val root = Project(id = id, base = file("."))
     .settings(commonSettings: _*)
     .settings(publishSettings: _*)
+    .settings(assemblySettings: _*)
     .settings(dependenciesSettings: _*)
 
 }
